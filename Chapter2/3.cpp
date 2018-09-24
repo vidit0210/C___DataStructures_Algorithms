05cc055402174caf089557eccd692a6a025176d7
vidit0210 <vidit0210@gmail.com>
2018-09-25 00:20:41 +0530

    Binary Search Iterative and Non-Iterative

--- Chapter2/3.cpp
+++ Chapter2/3.cpp
@@ -30,8 +30,9 @@ int recur_LinearSearch(vector<int> v,int low,int high,int value){
 }
 
 int recur_binarrySearch(vector<int> x ,int low,int high,int value){
-    if(low<high) return -1;
-    int mid = low+((high+low)/2);
+    if(low>high) return -1;
+    int mid = low+((high-low)/2);
+
     if(x[mid]==value) return mid;
     if(value>x[mid]) {
         low = mid+1;
@@ -41,10 +42,23 @@ int recur_binarrySearch(vector<int> x ,int low,int high,int value){
     return recur_binarrySearch(x,low,high,value);
 }
 
-
+int binary_Search(vector<int> x,int value){
+    int low =0;
+    int high = sizeof(x);
+    while(low<=high){
+        int mid  = low+ ((high-low)/2);
+        if(x[mid]==value) return mid;
+        if(value>mid) {
+            low = mid+1;
+        } else{
+            high= mid-1;
+        }
+    }
+    return -1;
+}
 
 
 int main(){
-    vector<int> x = {3,5,21,578,12,6};
-    cout<<recur_LinearSearch(x,0,4,5);
+    vector<int> x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+    cout<<binary_Search(x,3);
 }

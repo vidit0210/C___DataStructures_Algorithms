c39db623230fd9306cb5a354539432dc13fa0081
vidit0210 <vidit0210@gmail.com>
2018-09-24 23:52:53 +0530

    Linear Search

--- Chapter2/3.cpp
+++ Chapter2/3.cpp
@@ -19,7 +19,32 @@ int value(vector<int> arr,int val){
     }
     return -1;
 }
+
+//C++ recurcive Version-LinearSearch
+
+int recur_LinearSearch(vector<int> v,int low,int high,int value){
+  if(low>high) return -1;
+  if(v[low]==value) return low;
+  return recur_LinearSearch(v,low+1,high,value);
+
+}
+
+int recur_binarrySearch(vector<int> x ,int low,int high,int value){
+    if(low<high) return -1;
+    int mid = low+((high+low)/2);
+    if(x[mid]==value) return mid;
+    if(value>x[mid]) {
+        low = mid+1;
+    }else{
+        high = mid-1;
+    }
+    return recur_binarrySearch(x,low,high,value);
+}
+
+
+
+
 int main(){
     vector<int> x = {3,5,21,578,12,6};
-    cout<<value(x,121324);
+    cout<<recur_LinearSearch(x,0,4,5);
 }

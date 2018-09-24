cd86aea13c0cb7b3acbcf19f32f77523c281d147
vidit0210 <vidit0210@gmail.com>
2018-09-24 21:16:52 +0530

    Linear Search

--- Chapter2/3.cpp
+++ Chapter2/3.cpp
@@ -2,3 +2,24 @@
 // Created by vidit on 20/9/18.
 //
 
+// linear search
+
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+int value(vector<int> arr,int val){
+    for (int i =0;i< sizeof(arr);i++){
+        if(arr[i]==val){
+            return i;
+        }
+
+
+    }
+    return -1;
+}
+int main(){
+    vector<int> x = {3,5,21,578,12,6};
+    cout<<value(x,121324);
+}
